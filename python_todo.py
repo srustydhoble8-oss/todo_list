0f782b8d181b833654059e5b53ebf4440a6d875a
Universe_stars <srustydhoble8@gmail.com>
2026-06-19 20:05:48 +0530

    Update python_todo.py

--- python_todo.py
+++ python_todo.py
@@ -21,6 +21,7 @@ while True:
                 print("⚠️ Task cannot be empty!")
         else:
             print("⚠️ Task already exists!")
+            
     elif choice == "2":
         print("removed tasks ")
         removed_tasks = input("enetr task to remove: ")
@@ -29,29 +30,34 @@ while True:
             print("🗑️tasks removed! ",tasks)
         else:
             print("📪tasks not found")
+            
     elif choice == "3":
         print("your tasks ",tasks)
         if len(tasks) == 0:
             print("📪No tasks available!")
         else:
             # count the tasks
-            print("/n📋 Your Tasks: ")
+            print("\n📋 Your Tasks: ")
             count = 1
             for i in tasks:
                 print(count,".",i)
                 count += 1
+                
     elif choice == "4":
         tasks.clear()
-        print("🗑️ All tasks cleared!")            
+        print("🗑️ All tasks cleared!")  
+        
     elif choice == "5":
         search_task = input("Enter task to search: ")
         if search_task in tasks:
             print("✅ Task found!")
         else:
             print("📪 Task not found")
+            
     elif choice == "6":
         print("Thanks for using these app😄. Goodbye👋!")
         break
+        
     else:
         print("🚫Invalid option 🫥 Please choice again! 1,2,3,4,5 or 6")
          
